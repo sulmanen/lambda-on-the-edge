f02d834171e1730fd70f2215344a385638d3abbc
Samuli Ulmanen <samuli.ulmanen@wunderdog.fi>
2019-12-07 15:11:47 +0200

    start adding http header keys

--- src/lambdaAtEdge.ts
+++ src/lambdaAtEdge.ts
@@ -6,7 +6,59 @@ interface CloudFrontRecordConfig {
 }
 
 interface CloudFrontRequestBody {
+  action: 'read-only' | 'replace';
+  data: string;
+  encoding: 'text' | 'base64';
+  inputTruncated: boolean; // Body truncated by Lambda@Edge
+}
 
+interface CharSets { };
+interface MimeTypes { };
+interface Encodings { };
+interface DateTime { };
+interface Languages { };
+interface HTTPAuthorization { };
+interface HeaderCacheControl { };
+interface HTTPConnection { };
+interface Cookie { };
+interface HTTPExpect { };
+interface Forwarded;
+
+interface StandardHTTPRequestHeaders {
+  accept: MimeTypes[];
+  'accept-charset': CharSets;
+  'accept-encoding': Encodings;
+  'accept-language': Languages;
+  'accept-datetime': DateTime;
+  'access-control-request-method': HTTPMethod;
+  'access-control-request-headers': StandardHTTPRequestHeaders[];
+  authorization: HTTPAuthorization;
+  'cache-control': HeaderCacheControl;
+  connection: HTTPConnection;
+  'content-length': number;
+  'content-type': MimeTypes;
+  cookie: Cookie;
+  date: DateTime;
+  expect: HTTPExpect;
+  forwarded: Forwarded;
+  from: string;
+  host: string;
+  'if-match': string;
+  'if-modified-since': string;
+  'if-none-match': string;
+  'if-range': string;
+  'if-unmodified-since': DateTime;
+  'max-forwards': number;
+  origin: string;
+  pragma: string
+  'proxy-authorization': string;
+  range: string;
+  referer: string;
+  TE: string;
+  'user-agent': string;
+  upgrade: string;
+  via: string;
+  warning: string;
 }
 
 interface RequestHeaders {
@@ -17,12 +69,14 @@ interface RequestOrigin {
 
 }
 
+type HTTPMethod = 'GET' | 'POST' | 'DELETE' | 'OPTIONS' | 'HEAD';
+
 interface CloudFrontRequest {
   body: CloudFrontRequestBody;
   clientIp: string;
   querystring: string;
   uri: string;
-  method: 'GET' | 'POST' | 'DELETE' | 'OPTIONS' | 'HEAD';
+  method: HTTPMethod;
   headers: RequestHeaders;
   origin: RequestOrigin;
 }
