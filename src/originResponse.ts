c2b59c2f4fc506c12d7bf82e5bc1048d66d925df
Samuli Ulmanen <samuli.ulmanen@wunderdog.fi>
2021-01-22 08:16:41 +0200

    prerequisites

--- src/originResponse.ts
+++ src/originResponse.ts
@@ -4,7 +4,6 @@ import {
     LambdaResponseCallback
   } from './lambda@Edge';
 
-  // modify cache key
   export const handler = (event: Event, context: LambdaContext, callback: LambdaResponseCallback) => {
     event.Records[0].cf.request.headers.push({'x-random': {key: 'X-Random', value: Math.round(Math.random())}});
   };
