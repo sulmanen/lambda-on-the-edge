a6d0dba298168c5a4f03eb6c7c227bb4285696ea
Samuli Ulmanen <samuli.ulmanen@wunderdog.fi>
2021-02-05 13:41:10 +0200

    remove obsolete comment

--- src/viewerRequest.ts
+++ src/viewerRequest.ts
@@ -12,9 +12,6 @@ const split = (userId: string) => {
   return parseInt(hash.charAt(0), 16) % 2;
 }
 
-/*
-  Incoming user id. Divide users into two groups by hashing user id and setting a new cookie.
-*/
 export const handler = (event: Event, context: LambdaContext | null, callback: LambdaRequestCallback) => {
   const { request } = event.Records[0].cf;
 
